752fff9ccec0d744adbd4bba1452ef1cee0d1d39
dmm00093 <dmm00093@red.ujaen.es>
2026-05-26 16:57:04 +0200

    (edit ficheros lib) Struct de configuración añadido (STM+Prods)

--- lib/ficheros.c
+++ lib/ficheros.c
@@ -26,29 +26,34 @@ int cargar_texto(const char *ruta, Producto *arr, int cap, int *out_n)
         // ptemp puede ser: ptemp = {2, "Coca-Cola", 1.50, 5}. Si no falla, bien, si falla, pues, nos ahorramos meter basura
         // Y una vez tenemos eso, dentro de mi array de productos pues metemos esa p en mi posicion arr[n], que tiene n++
         // Entonces pues copiamos ese array temporal ahi dentro
-        // p es un molde reutilizable que se rellena y se copia al array.
+        // p es  reutilizable, se rellena y se copia al array.
 
         if (sscanf(linea, "%d;%31[^;];%f;%d", &pTemp.id, pTemp.nombre, &pTemp.precio, &pTemp.stock) == 4)
         {
             arr[n++] = pTemp;
         }
+		
     } fclose(f); // Cerramos fichero.
 
     *out_n = n;
     return 0;
 }
 
-int cargar_binario(const char *ruta_input, char *ruta_output)
+int cargar_binario(const char *ruta_input, Configuracion *ruta_output) // Usamos el struct para el output
 {
     // ruta input es predefinida. ruta output es dinamica, nos dira donde está para que el programa lo use.
     FILE *f = fopen(ruta_input, "rb");
 
     if (!f) {
         //No se pudo cargar ruta.
-        return 1;
+        return -1;
     }
 
-    fread(ruta_output, sizeof(char), 256, f); // leemos ruta salida
+    fread(ruta_output, sizeof(Configuracion), 1, f); // leemos ruta salida de tamaño del struct.
+													 // 1->cantidad de elementos, que será solo 1. 	
+
+	// Si usaramos un char, vale claro. Hacemos 256, porque introducimos 256 chars de 1 byte, que son, 256 bytes, guay.
+	// Pero como usaremos un struct, no queremos meter 256 structs. Eso es inviable, la memoria, kaput.
 
     fclose(f);
 
@@ -60,7 +65,7 @@ int guardar_texto(const char *ruta, const Producto *arr, int n)
     FILE *f = fopen(ruta, "w");
     if (!f) {
         perror("ERROR. Motivo de fallo: ");
-        return 1;
+        return -1;
     }
 
     for (int i = 0 ; i < n; i++) {
@@ -72,16 +77,16 @@ int guardar_texto(const char *ruta, const Producto *arr, int n)
     return 0;
 }
 
-int guardar_binario(const char *ruta_input, char *ruta_output)
+int guardar_binario(const char *ruta_input, Configuracion *ruta_output)
 {
     FILE *f = fopen(ruta_input, "wb"); // apertura en binario write
 
     if (!f) {
         printf("No se pudo cargar. "); perror("Error:");
-        return 1;
+        return -1;
     }
 
-    fwrite(ruta_output, sizeof(char), 256, f); // igual que cargar con fwrite y wb.
+    fwrite(ruta_output, sizeof(Configuracion), 1, f); // igual que cargar, pero con fwrite y wb.
 
     fclose(f);
     return 0;
--- lib/ficheros.h
+++ lib/ficheros.h
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <string.h> // Necesario para strlen
 
-// 1. Cremaos el struct productos en el .h para que se pueda usar
+// Definiremos el struct de productos aquí, para luego no tener que ir definiendo structs en el main
+// Esto no es que sea muy intuitivo pero para lo que nosotros queremos simplifica mucho el main...
+
 typedef struct {
     int id;
     char nombre[32]; // Asumo este tamaño por tu "%31[^;]" en sscanf
@@ -12,18 +14,25 @@ typedef struct {
     int stock;
 } Producto;
 
-// 2. Creamos el struct de configuracion. Esto es mas versatil para todo
+// Definiremos un struct de CONFIGURACION aquí. Ya que tenemos config de la maquina y del STM.
+// Así me ahorro configOK y blabla veinte veces y hacer un cargar_binario,texto otras 20 veces.
 
+#pragma pack (push, 1) // <-- Esto es Gemini. El compilador puede meterme bytes de relleno y joderlo todo.
 typedef struct {
-    char ruta_productos;
-    int puertoCOM;
+    char ruta_productos [256];
+    char puertoCOM [20];
     int baudios;
+	int configOK; // Esto es algo local. En vez de definir un int en mi main veremos este int en el struct.
+				  // La idea no fue mia, fue por gemini. Porque me estaba volviendo loco haciendo todo otra vez en el main.
+				  // configOK es 0 -> OK todo 
+				  // configOK es .1 -> kaput
 } Configuracion;
+#pragma pack(pop)
 
 // 2. Funciones
 int cargar_texto(const char *ruta, Producto *arr, int cap, int *out_n);
-int cargar_binario(const char *ruta_input, char *ruta_output);
+int cargar_binario(const char *ruta_input, Configuracion *ruta_output);
 int guardar_texto(const char *ruta, const Producto *arr, int n);
-int guardar_binario(const char *ruta_input, char *ruta_output);
+int guardar_binario(const char *ruta_input, Configuracion *ruta_output);
 
 #endif // ARCHIVOS_H
